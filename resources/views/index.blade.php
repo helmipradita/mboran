6e32db3839e58df409314616b1a7f11912653cde
Helmi Pradita <helmipraditaa@gmail.com>
2022-11-09 22:12:31 +0700

    change index

---
+++ resources/views/index.blade.php
@@ -0,0 +1,23 @@
+@extends('layouts.frontend')
+
+@section('content')
+    <div class="container">
+        <div class="text-center">
+            <h1>LOGO MBORAN</h1>
+        </div>
+
+        <div class="col-md-5">
+            <h3>Belum Ke Lamongan Kalau Belum Makan Nasi Boranan</h3>
+        </div>
+
+        <div class="col-md-4">
+            <a href="/login" class="btn btn-secondary">Masuk</a>
+            <a href="/register" class="btn btn-secondary">Daftar</a>
+        </div>
+        
+        <br>
+        <div class="col-md-4">
+            <a href="/sejarah" class="btn btn-primary">Kenali Nasi Boranan</a>
+        </div>
+    </div>
+@endsection
