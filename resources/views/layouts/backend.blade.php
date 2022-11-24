b3bbfc5df2ef1c21d95dee7c0518f83f544296b8
Helmi Pradita <helmipraditaa@gmail.com>
2022-11-24 15:08:48 +0700

    fix dashboard navbar

--- resources/views/layouts/backend.blade.php
+++ resources/views/layouts/backend.blade.php
@@ -18,61 +18,54 @@
     <title>Mboran</title>
   </head>
   <body>
-    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-        <div class="container">
-            <a class="navbar-brand" href="/">Mboran</a>
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-              <span class="navbar-toggler-icon"></span>
-            </button>
-        
-            <div class="collapse navbar-collapse" id="navbarSupportedContent">
-              <ul class="navbar-nav mr-auto">
-                <li class="nav-item">
-                  <a class="nav-link" href="/dashboard">Dashboard</span></a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="{{ route('kecamatan.index') }}">Kecamatan</span></a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="{{ route('penjual.index') }}">Penjual</span></a>
-                </li>
-              </ul>
-            </div>
+    <div id="app">
+      <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+          <div class="container">
+              <a class="navbar-brand" href="/">Mboran</a>
+              <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+              </button>
 
-            <div class="collapse navbar-collapse" id="navbarSupportedContent">
-              <!-- Left Side Of Navbar -->
-              <ul class="navbar-nav me-auto">
+              <div class="collapse navbar-collapse" id="navbarText">
+                <ul class="navbar-nav mr-auto">
+                  <li class="nav-item">
+                    <a class="nav-link" href="/dashboard">Dashboard</span></a>
+                  </li>
+                  <li class="nav-item">
+                      <a class="nav-link" href="{{ route('kecamatan.index') }}">Kecamatan</span></a>
+                  </li>
+                  <li class="nav-item">
+                      <a class="nav-link" href="{{ route('penjual.index') }}">Penjual</span></a>
+                  </li>
+                </ul>
+                <ul class="navbar-nav ms-auto">
+                  <li class="nav-item dropdown">
+                      <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                          {{ Auth::user()->name }}
+                      </a>
 
-              </ul>
+                      <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
+                          <a class="dropdown-item" href="{{ route('logout') }}"
+                            onclick="event.preventDefault();
+                                          document.getElementById('logout-form').submit();">
+                              {{ __('Logout') }}
+                          </a>
 
-              <!-- Right Side Of Navbar -->
-              <ul class="navbar-nav ms-auto">
-                <li class="nav-item dropdown">
-                    <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                        {{ Auth::user()->name }}
-                    </a>
-
-                    <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
-                        <a class="dropdown-item" href="{{ route('logout') }}"
-                          onclick="event.preventDefault();
-                                        document.getElementById('logout-form').submit();">
-                            {{ __('Logout') }}
-                        </a>
-
-                        <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
-                            @csrf
-                        </form>
-                    </div>
-                </li>
-              </ul>
+                          <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                              @csrf
+                          </form>
+                      </div>
+                  </li>
+                </ul>
+              </div>
           </div>
-        </div>
-    </nav>
-
-    <div class="container">
-        <br>
-        @yield('content')
-    </div>
+      </nav>
+      
+      <div class="container">
+          <br>
+          @yield('content')
+      </div>
+    <div id="app">
 
     <!-- Optional JavaScript -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
