65266d09949588e0dce188032da4c30df865b56e
Helmi Pradita <helmipraditaa@gmail.com>
2022-11-24 15:33:33 +0700

    change layout form kecamatan

--- resources/views/dashboard/kecamatan/index.blade.php
+++ resources/views/dashboard/kecamatan/index.blade.php
@@ -9,46 +9,53 @@
         </div>
     @endif
     
-    <div class="mb-4">
-        <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
-            <h1 class="h2">Tambah Kecamatan</h1>
+    <div class="row">
+        <div class="col-md-6">
+            <div class="mb-4">
+                <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
+                    <h1 class="h2">Tambah Kecamatan</h1>
+                </div>
+                <div class="">
+                    <form action="{{ route('kecamatan.create') }}" method="post">
+                        @csrf
+                        @include('dashboard.kecamatan.partials.form-control', ['submit' => 'Tambah'])
+                    </form>
+                </div>
+            </div>
         </div>
-        <div class="">
-            <form action="{{ route('kecamatan.create') }}" method="post">
-                @csrf
-                @include('dashboard.kecamatan.partials.form-control', ['submit' => 'Tambah'])
-            </form>
-        </div>
-    </div>
-    <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
-        <h1 class="h2">List Kecamatan</h1>
-    </div>
+        
+        <div class="col-md-6">
+            <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
+                <h1 class="h2">List Kecamatan</h1>
+            </div>
 
-    <div class="table-responsive">
-        <table class="table table-striped table-sm">
-            <thead>
-                <tr>
-                    <th>#</th>
-                    <th>Kecamatan</th>
-                    <th>Warna</th>
-                    <th>Action</th>
-                </tr>
-            </thead>
-            <tbody>
-                @foreach($kecamatan as $index => $data)
-                    <tr>
-                        <td>{{ $index + 1 }}</td>
-                        <td>{{ $data->kecamatan }}</td>
-                        <td style="background-color: {{ $data->warna }}"></td>
-                        <td>
-                            <a href="kecamatan/edit/{{ $data->id_kecamatan }}" class="btn btn-primary">Edit</a>
-                            
-                            <a href="kecamatan/delete/{{ $data->id_kecamatan }}" onclick="return confirm('Apakah Anda Yakin Menghapus Data?');" class="btn btn-danger">Delete</a>
-                        </td>
-                    </tr>
-                @endforeach
-            </tbody>
-        </table>
+            <div class="table-responsive">
+                <table class="table table-striped table-sm">
+                    <thead>
+                        <tr>
+                            <th>#</th>
+                            <th>Kecamatan</th>
+                            <th>Warna</th>
+                            <th>Action</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @foreach($kecamatan as $index => $data)
+                            <tr>
+                                <td>{{ $index + 1 }}</td>
+                                <td>{{ $data->kecamatan }}</td>
+                                <td style="background-color: {{ $data->warna }}"></td>
+                                <td>
+                                    <a href="kecamatan/edit/{{ $data->id_kecamatan }}" class="btn btn-primary">Edit</a>
+                                    
+                                    <a href="kecamatan/delete/{{ $data->id_kecamatan }}" onclick="return confirm('Apakah Anda Yakin Menghapus Data?');" class="btn btn-danger">Delete</a>
+                                </td>
+                            </tr>
+                        @endforeach
+                    </tbody>
+                </table>
+            </div>
+        </div>
     </div>
 
     {{-- <script>
